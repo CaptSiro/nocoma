7c71af7cd92356ce1e7c56f0b257d51a0381ac73
CaptSiro <honzahorak433@icloud.com>
2022-09-15 19:35:31 +0200

    minor changes to modelist library

--- lib/modelist/src/insert.sql
+++ lib/modelist/src/insert.sql
@@ -1,3 +1,3 @@
 INSERT INTO themes (`name`) VALUE ("default");
 INSERT INTO users (`themesID`, `email`, `password`, `level`, `website`, `isVerified`)
-VALUE (1, "admin@nocoma.com", "$2y$10$/x1dW6CcTZcqxm.H6YT5m.CJA.WSFwhe4DhhHqOyMQp2nkXECtqUS", 0, "admin", 1);
+VALUE (1, "admin@nocoma.com", "$2y$10$R13kOWVeLFsmhvUkFdA0qebmBBLPbVSfJD18V5AKJS1AKVaiQWap6", 0, "admin", 1);

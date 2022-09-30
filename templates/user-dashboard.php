1555afb81c059ac276261c3e675369bd71308d3b
CaptSiro <honzahorak433@icloud.com>
2022-09-30 11:55:46 +0200

    1st dashboard draft design

--- templates/user-dashboard.php
+++ templates/user-dashboard.php
@@ -11,9 +11,43 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Dashboard - NoComa</title>
+
+  <link rel="stylesheet" href="../public/css/main.css">
+  <link rel="stylesheet" href="../public/css/dashboard.css">
 </head>
 <body>
-  <p>YOU MADE IT HERE AT LAST!</p>
-  <a href="../ajax/get/delete-logout.php">Logout</a>
+  <!-- <a href="../ajax/get/delete-logout.php">&lt;-</a> -->
+  <nav>
+    <section>
+      <a href="">An</a>
+      <a href="">Po</a>
+      <a href="">Co</a>
+      <a href="">Th</a>
+      <a href="">Ga</a>
+      <a href="">Pr</a>
+    </section>
+    <section>
+      <a href="">&lt;-</a>
+    </section>
+  </nav>
+  <main>
+    <section>
+      <header>
+        <button>Some stuff</button>
+        <button>More stuff</button>
+      </header>
+      <div class="scrollable">
+        <p>Long ass text</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus, qui autem. Eveniet tempora optio perspiciatis omnis unde beatae quam laborum, cupiditate quasi similique sapiente possimus consequatur id consectetur dicta qui!</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus, qui autem. Eveniet tempora optio perspiciatis omnis unde beatae quam laborum, cupiditate quasi similique sapiente possimus consequatur id consectetur dicta qui!</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus, qui autem. Eveniet tempora optio perspiciatis omnis unde beatae quam laborum, cupiditate quasi similique sapiente possimus consequatur id consectetur dicta qui!</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus, qui autem. Eveniet tempora optio perspiciatis omnis unde beatae quam laborum, cupiditate quasi similique sapiente possimus consequatur id consectetur dicta qui!</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus, qui autem. Eveniet tempora optio perspiciatis omnis unde beatae quam laborum, cupiditate quasi similique sapiente possimus consequatur id consectetur dicta qui!</p>
+      </div>
+    </section>
+    <section>
+
+    </section>
+  </main>
 </body>
 </html>

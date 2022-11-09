6a3d053dc009763b30804c3e626f3e206ac099eb
CaptSiro <honzahorak433@icloud.com>
2022-11-09 08:43:43 +0100

    .env file parser

---
+++ lib/dotenv/dotenv.php
@@ -0,0 +1,20 @@
+<?php
+
+  class Env {
+    private $__map = [];
+    function __construct ($file) {
+      $handle = fopen($file, "r");
+      if ($handle) {
+        while (($line = fgets($handle)) !== false) {
+          preg_match("/(.*)=(.*)./", $line, $matches);
+          $this->__map[$matches[1]] = $matches[2];
+        }
+
+        fclose($handle);
+      }
+    }
+
+    function __get ($name) {
+      return $this->__map[$name];
+    }
+  }

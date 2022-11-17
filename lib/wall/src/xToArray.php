2cdf64714d37b4f7f27f1b032d011505ce28110e
CaptSiro <honzahorak433@icloud.com>
2022-11-17 15:01:02 +0100

    Removed legacy library 'wall'

--- lib/wall/src/xToArray.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-  function xToArray ($x, array &$appendTo = null): array {
-    $a = isset($appendTo) ? $appendTo : [];
-    foreach ($x as $k => $v) {
-      $a[$k] = $v;
-    }
-    return $a;
-  }

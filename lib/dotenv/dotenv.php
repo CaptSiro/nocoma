3a26e86545efe73917e9c9f4363bce3510d174c9
CaptSiro <honzahorak433@icloud.com>
2022-11-17 11:50:06 +0100

    Added result-returning method get

--- lib/dotenv/dotenv.php
+++ lib/dotenv/dotenv.php
@@ -1,12 +1,16 @@
 <?php
 
+  require_once __DIR__ . "/../retval/retval.php";
+
   class Env {
     private $__map = [];
     function __construct ($file) {
+      $this->__map["__ENV_FILE__"] = $file;
+
       $handle = fopen($file, "r");
       if ($handle) {
         while (($line = fgets($handle)) !== false) {
-          preg_match("/(.*)=(.*)/", rtrim($line, "\r\n"), $matches);
+          preg_match("/(.*)=(.*).?#?.*/", rtrim($line, "\r\n"), $matches);
           $this->__map[$matches[1]] = $matches[2];
         }
 
@@ -14,7 +18,19 @@
       }
     }
 
+    public function get ($name): Result {
+      if (!isset($this->__map[$name])) {
+        return fail(new NotFoundExc("Could not find $name in " . $this->__map["__ENV_FILE__"]));
+      }
+  
+      return success($this->__map[$name]);
+    }
+    
     function __get ($name) {
+      if (!isset($this->__map[$name])) {
+        return null;
+      }
+  
       return $this->__map[$name];
     }
   }

59fa5084dae3b88c679b70e88efaf66665dca34c
CaptSiro <honzahorak433@icloud.com>
2022-11-23 08:30:01 +0100

    widgets resource getters

---
+++ ajax/get/widget-file.php
@@ -0,0 +1,26 @@
+<?php
+
+  require_once __DIR__ . "/../../lib/rekves/rekves.php";
+  require_once __DIR__ . "/../../lib/structure/structure.php";
+  require_once __DIR__ . "/../../lib/dotenv/dotenv.php";
+  require_once __DIR__ . "/../../lib/paths.php";
+
+  $env = new Env(ENV_FILE);
+  $propsRes = $env->get("WIDGETS_DIR");
+  $propsRes->forwardFailure($res);
+
+  $parser = new Parser($_SERVER["DOCUMENT_ROOT"] . $propsRes->getSuccess());
+
+  if (!isset($parser->registry[$req->body->c])) {
+    $res->error("Could not find widget by class '" . $req->body->c . "'.", Response::NOT_FOUND);
+  }
+
+  $stripped = $parser->registry[$req->body->c]->stripPrivate(false);
+  if (!isset($stripped->files[$req->body->get("f", "icon")])) {
+    $res->error("Could not find file '" . $req->body->get("f", "icon") . "' in files array.", Response::NOT_FOUND);
+  }
+
+  $fileName = $stripped->files[$req->body->get("f", "icon")];
+  $res->setHeader("Content-type", mime_content_type($fileName));
+  readfile($fileName);
+  $res->flush();
---
+++ ajax/get/widget-resource.php
@@ -0,0 +1,32 @@
+<?php
+
+  require_once __DIR__ . "/../../lib/rekves/rekves.php";
+  require_once __DIR__ . "/../../lib/structure/structure.php";
+  require_once __DIR__ . "/../../lib/dotenv/dotenv.php";
+  require_once __DIR__ . "/../../lib/paths.php";
+
+  $classes = explode(",", $req->body->c);
+
+  $env = new Env(ENV_FILE);
+  $propsRes = $env->get("WIDGETS_DIR");
+  $propsRes->forwardFailure($res);
+
+  $parser = new Parser($_SERVER["DOCUMENT_ROOT"] . $propsRes->getSuccess());
+
+  
+  $prepared = [];
+  if ($classes[0] == "*") {
+    foreach ($parser->registry as $key => $value) {
+      $prepared[] = $value->stripPrivate();
+    }
+
+    $res->json($prepared);
+  }
+
+  foreach ($classes as $c) {
+    if (isset($parser->registry[$c])) {
+      $prepared[] = $parser->registry[$c]->stripPrivate();
+    }
+  }
+
+  $res->json($prepared);

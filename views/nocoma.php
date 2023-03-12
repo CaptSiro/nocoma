c1bbecf66c3707f6329828b87f6da506a4d58427
CaptSiro <honzahorak433@icloud.com>
2023-03-12 11:14:06 +0100

    Fixed accessibility issues and SEO

--- views/nocoma.php
+++ views/nocoma.php
@@ -4,6 +4,9 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="The user creates a domain under which he/she then manages the articles of his/her blog. Each article is treated as a web page that will be editable. It is possible to choose a theme for the entire domain.">
+  <meta name="keywords" content="domain, blog, articles, CMS, personification, themes, design">
+  
   <title>Nocoma</title>
   
   <script>

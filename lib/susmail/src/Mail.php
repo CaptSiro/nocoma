40a177ea73c067aca9752eb9c20bd45307d91f6d
CaptSiro <honzahorak433@icloud.com>
2023-03-18 17:21:45 +0100

    Fixed not sending mails to gmail

--- lib/susmail/src/Mail.php
+++ lib/susmail/src/Mail.php
@@ -6,9 +6,9 @@
     public static function message ($to, $subject, $msg) {
       $headers = "MIME-Version: 1.0" . "\r\n";
       $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-
       $headers .= 'From: <' . self::FROM . '>' . "\r\n";
+      $headers .= 'Reply-To: <' . self::FROM . '>' . "\r\n";
 
-      mail($to, $subject, $msg, $headers);
+      mail("<$to>", $subject, $msg, $headers);
     }
   }
